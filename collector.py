82fc94e8b00608b08b06ec84d19caa707c4efe3c
alexandrebarbaruiva <alexandrebarbaruiva@gmail.com>
2019-05-25 12:32:13 -0300

    Finish algorithm

--- collector.py
+++ collector.py
@@ -1,1 +1,23 @@
+from twitter_scraper import get_tweets
+import csv
 
+
+with open('pages.txt', 'r') as pages:
+    pages = pages.readlines()
+
+for page in pages:
+    tweets = get_tweets(page)
+    filename = f'pages/{page}.csv'
+    with open(filename, 'w') as pagefile:
+        writer = csv.writer(pagefile)
+        print('a')
+        try:
+            head = ['Date', 'Text', 'Classification']
+            writer.writerow(head)
+            print('b')
+            for tweet in tweets:
+                row = [tweet.get('time'), tweet.get('text'), '']
+                writer.writerow(row)
+                print('c')
+        except Exception as e:
+            print(e)
